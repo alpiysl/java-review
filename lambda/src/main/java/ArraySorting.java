2ddab114446e01cd8d7206233e5d8f549251b4d3
alpiysl <alpiysll@gmail.com>
2022-02-13 23:28:54 +0300

    behaviour param

---
+++ lambda/src/main/java/ArraySorting.java
@@ -0,0 +1,17 @@
+public class ArraySorting {
+
+    public static void main(String[] args) {
+
+        ArraySorting as = new ArraySorting();
+//        as.sort();
+        QuickSort quickSort = new QuickSort();
+        BubbleSort bubbleSort = new BubbleSort();
+        as.sort(quickSort);
+        as.sort(bubbleSort);
+    }
+
+    private void sort(Sorting sorting) {
+        sorting.sort();
+    }
+}
+
