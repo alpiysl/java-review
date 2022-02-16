a0b839899d5f0bddfcaa3e56bdfd7bfb9ba46c0b
alpiysl <alpiysll@gmail.com>
2022-02-17 01:22:44 +0300

    task solved

---
+++ Streams/src/main/java/Task.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Task {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("JAVA","HONDA","APPLE");
+        words.stream().
+                map(String::length).
+                forEach(System.out::println);
+
+    }
+}
